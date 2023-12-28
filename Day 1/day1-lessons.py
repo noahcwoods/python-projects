13bec5171c4587ec0a5e3b17c74dac5c5eca43f9
Noah <noahcwoods@gmail.com>
2023-12-28 14:17:47 -0500

    Completed Day 1

---
+++ Day 1/day1-lessons.py
@@ -0,0 +1,18 @@
+# print("Hello World!\nThis is my first\npython day")
+# print("Hello" + " Noah")
+
+# name = input("What is your name:  ")
+# print("Hello " + name + "")
+
+# print("Your last name is:  " + input("What is your last name:  "))
+
+#print("Enter your name:  ")
+#name = input()
+#length = len(name)
+#print("The total length of your name is: " + str(length))
+
+#name = input("Enter your name:  ")
+#print("The total length of your name is: " + str(len(name)))
+
+nameLength = len(input("Enter your name:  "))
+print(nameLength)
---
+++ Day 1/day1-project.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator!\n\n")
+
+cityName = input("Enter the city you grew up in:  ")
+petName = input("Enter the name of your first pet:  ")
+
+print("Your band name is: ",cityName,petName)
