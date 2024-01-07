c7d52af600f4f64936d260ed8b492798c21c8efb
Noah <noahcwoods@gmail.com>
2024-01-06 21:52:44 -0500

    Day 18 Progress

---
+++ Intermediate/Day 18/main.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("coral")
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
