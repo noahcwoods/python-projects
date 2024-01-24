297db9424a1c77c36aa28969871cc2c86e7dbd4b
Noah Woods <noahcwoods@gmail.com>
2024-01-23 21:28:32 -0500

    Day 31 Capstone
    Continued work on the application. Added functionality to the buttons and added the card flipping after a 3 second timer on each new word

--- Intermediate/Day 31/main.py
+++ Intermediate/Day 31/main.py
@@ -12,43 +12,74 @@ CORRECT = PhotoImage(file="images/right.png")
 WRONG = PhotoImage(file="images/wrong.png")
 DATA = pd.read_csv("data/french_words.csv")
 
-_french_word = None
-_english_word = None
-_random_index = None
+to_learn = DATA.to_dict(orient="records")
+_card_front = True
+timer = None
+current_word = None
 
-# POPULATE DATA ###############################################################
 
-# FRENCH SELECTION
+# WORD SELECTION
 def select_word():
-    global _french_word
-    global _english_word
-    global _random_index
+    global timer
+    global current_word
+    current_word = random.choice(to_learn)
+    canvas.itemconfig(_displayed_language, text="French")
+    _displayed_word.config(text=current_word["French"])
 
-    _random_index = random.randint(0, len(DATA))
-
-    try:
-        _french_word = DATA.French[_random_index]
-        _english_word = DATA.English[_random_index]
-    except KeyError:
-        print("French Word Selection Error: Index out of range")
+    timer = master.after(3000, flip_card)
 
 
-# UI SETUP ###############################################################
+def new_word():
+    global _card_front
+    master.after_cancel(timer)
+
+    if not _card_front:
+        flip_card()
+    select_word()
+
+
+# Correct Button Event
+def on_correct_selection():
+    new_word()
+
+
+# Wrong Button Event
+def on_wrong_selection():
+    new_word()
+
+
+def flip_card():
+    global _card_front
+    if _card_front:
+        canvas.itemconfig(_card_displayed, image=FLASHCARD_BACK)
+        canvas.itemconfig(_displayed_language, fill="white", text="English")
+        _displayed_word.config(fg="white", bg="#91C2AF", text=current_word["English"])
+
+        _card_front = False
+
+    else:
+        canvas.itemconfig(_card_displayed, image=FLASHCARD_FRONT)
+        canvas.itemconfig(_displayed_language, fill="black", text="French")
+        _displayed_word.config(fg="black", bg="white")
+        _card_front = True
+
+
+# UI CONFIGURATION
 master.title("Flashy!")
 master.minsize(width=900, height=626)
 master.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(master)
 canvas.config(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-
+_card_displayed = canvas.create_image(400, 263, image=FLASHCARD_FRONT)
 
 # LABELS
 _displayed_language = canvas.create_text(400, 150, text="LANGUAGE", font=MAIN_FONT_ITALIC)
-_displayed_word = Label(master, text="PLACEHOLDER", font=MAIN_FONT, fg="black")
+_displayed_word = Label(master, text="PLACEHOLDER", font=MAIN_FONT, fg="black", bg="white")
 
 # BUTTONS
-correct_button = Button(master, text="", image=CORRECT, highlightthickness=0)
-wrong_button = Button(master, text="", image=WRONG, highlightthickness=0)
+correct_button = Button(master, text="", image=CORRECT, highlightthickness=0, command=on_correct_selection)
+wrong_button = Button(master, text="", image=WRONG, highlightthickness=0, command=on_wrong_selection)
 
 # GRID CONFIGURATION
 canvas.grid(row=0, column=0, columnspan=2)
@@ -57,7 +88,6 @@ _displayed_word.grid(row=0, column=0, columnspan=2)
 
 correct_button.grid(row=1, column=0)
 wrong_button.grid(row=1, column=1)
-
 
 select_word()
 
